8f1b4f016ee071663d845606e8f19e654c547621
Ben <ei18017@bristol.ac.uk>
2021-05-04 14:08:36 +0100

    Fixed Charlies code cos it didn't work

---
+++ NLP/checkIngredients.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from webscraping import sqlDatabaseManagement as sql
+
+
+def removeDuplicates(myList):
+    return list(dict.fromkeys(myList))
+
+
+def checkIngredients(recipeDatabaseConn, userIngredientsArray):
+    IDArray = sql.sqlGetCol(recipeDatabaseConn, "ID")
+    possibleRecipesID = []
+    for ID in IDArray:
+        recipeCleanIngredients = sql.sqlGetSpecificID(recipeDatabaseConn, "CLEANINGREDIENTS", ID)[0].split(", ")
+        if set.issubset(set(recipeCleanIngredients), set(userIngredientsArray)):
+            possibleRecipesID.append(ID)
+
+    return possibleRecipesID
+
