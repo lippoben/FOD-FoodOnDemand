07dc7e675232090d9ef66669f55cb344b1426ca8
Ben <ei18017@bristol.ac.uk>
2021-05-04 15:48:36 +0100

    Had to change indexing the output of sqlGetSpecficID as this now happens in the function itself

--- NLP/checkIngredients.py
+++ NLP/checkIngredients.py
@@ -9,7 +9,7 @@ def checkIngredients(recipeDatabaseConn, userIngredientsArray):
     IDArray = sql.sqlGetCol(recipeDatabaseConn, "ID")
     possibleRecipesID = []
     for ID in IDArray:
-        recipeCleanIngredients = sql.sqlGetSpecificID(recipeDatabaseConn, "CLEANINGREDIENTS", ID)[0].split(", ")
+        recipeCleanIngredients = sql.sqlGetSpecificID(recipeDatabaseConn, "CLEANINGREDIENTS", ID).split(", ")
         if set.issubset(set(recipeCleanIngredients), set(userIngredientsArray)):
             possibleRecipesID.append(ID)
 
