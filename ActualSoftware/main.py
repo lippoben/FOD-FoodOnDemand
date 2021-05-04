5a1454200f987b889de619195e600c7af7a64c83
Ben <ei18017@bristol.ac.uk>
2021-05-04 13:36:24 +0100

    Produced some very very basic pesudo for the structure of program

--- ActualSoftware/main.py
+++ ActualSoftware/main.py
@@ -1,1 +1,15 @@
+from webscraping import sqlDatabaseManagement as sql
+from Veganiser import veganiser
+from recipeGreenLighter import checkIngredients
 
+# this will be what the user has in stock
+userInput = []
+
+# Green light recipes here
+
+
+# Check Green lit recipes here against user's preference profile and select the recipes that best match
+
+# Veganise Recipes
+
+# Display output to the user
