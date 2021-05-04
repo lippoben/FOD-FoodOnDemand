c210c015305909ad941bb1629b92415ec4a52ae2
Ben <ei18017@bristol.ac.uk>
2021-05-04 14:44:21 +0100

    Updated sqlite3 to csv.py to be usable as a function in other python if it is needed

--- webscraping/sqlite3 to csv.py
+++ webscraping/sqlite3 to csv.py
@@ -1,20 +1,20 @@
 import sqlDatabaseManagement as sql
 import pandas as pd
-import numpy as np
 
-conn = sql.sqlInit('recipeDatabase.db')
-uniqueIDTotal = np.linspace(1, sql.sqlCount(conn)[0], sql.sqlCount(conn)[0])
-colNameArray = sql.sqlGetColNames(conn)
 
-colEntriesArray = []
-for colName in colNameArray:
-    colEntries = sql.sqlGetCol(conn, colName)
-    colEntriesArray.append(colEntries)
+def dbToCSV(dbFileName, csvFileName):
+    conn = sql.sqlInit(dbFileName)
+    colNameArray = sql.sqlGetColNames(conn)
 
-d = {colNameArray[0]: colEntriesArray[0], colNameArray[1]: colEntriesArray[1], colNameArray[2]: colEntriesArray[2]
-    , colNameArray[3]: colEntriesArray[3], colNameArray[4]: colEntriesArray[4],
-     colNameArray[5]: colEntriesArray[5], colNameArray[6]: colEntriesArray[6]}
+    colEntriesArray = []
+    for colName in colNameArray:
+        colEntries = sql.sqlGetCol(conn, colName)
+        colEntriesArray.append(colEntries)
 
-CSVFileDF = pd.DataFrame(d)
+    d = {colNameArray[0]: colEntriesArray[0], colNameArray[1]: colEntriesArray[1], colNameArray[2]: colEntriesArray[2]
+        , colNameArray[3]: colEntriesArray[3], colNameArray[4]: colEntriesArray[4],
+         colNameArray[5]: colEntriesArray[5], colNameArray[6]: colEntriesArray[6]}
 
-CSVFileDF.to_csv('C:/Users/lipb1/Documents/Year 3 Bristol/MDM3/FOD/webscraping/recipeDatabase.csv')
+    CSVFileDF = pd.DataFrame(d)
+
+    CSVFileDF.to_csv(csvFileName)
