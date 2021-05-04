aa177fc21390427f82199d77548dd44cb3a260d1
Ben <ei18017@bristol.ac.uk>
2021-05-04 13:33:11 +0100

    Made the green lighting ingredients code into a python directory making in importable anywhere. Also made the checkIngredients.py into a module i.e taking out all globals and making everything else take paramaters. Also fixed variable naming conventions to follow PEP8

---
+++ recipeGreenLighter/checkIngredients.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from webscraping import sqlDatabaseManagement as sql
+
+
+def removeDuplicates(myList):
+    return list(dict.fromkeys(myList))
+
+
+def checkIngredients(recipeDatabaseConn, randomIngredientsDf):
+    IDArray = sql.sqlGetCol(recipeDatabaseConn, "ID")
+    possibleRecipesID = []
+
+    for ID in IDArray:
+        cleanIngredients = sql.sqlGetSpecificID(recipeDatabaseConn, "CLEANINGREDIENTS", ID)[0].split(", ")
+        for j in range(0, len(cleanIngredients)):
+            if len(randomIngredientsDf[randomIngredientsDf['Ingredients'] == cleanIngredients[j]]) == 0:
+                break
+            possibleRecipesID.append(ID)
+
+    possibleRecipesID = removeDuplicates(possibleRecipesID)
+
+    print(possibleRecipesID)
+    print(len(possibleRecipesID))
+
