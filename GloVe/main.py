e491e2f5dcf6026f55b9efddc88ea9de49e48d80
Ben <ei18017@bristol.ac.uk>
2021-05-07 11:41:31 +0100

    Got Glove model ouputting nearest 3 ingredients and functionised it for use in the actual software

--- GloVe/main.py
+++ GloVe/main.py
@@ -2,6 +2,8 @@ import re
 import os
 
 import numpy as np
+import pandas as pd
+from scipy import spatial
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -31,36 +33,108 @@ def getUniqueWords(sentence):
     return sentence.split()
 
 
-def trainModel():
-    corpusTextFileNameArray = os.listdir('corpora/methodCorpus/')
+def trainModel(model, corpusDir, epochs=30):
+    corpusTextFileNameArray = os.listdir(corpusDir)
     corpusTextArray = []
     for corpusTextFileName in corpusTextFileNameArray:
-        with open('corpora/methodCorpus/' + corpusTextFileName, 'r') as f:
+        with open(corpusDir + corpusTextFileName, 'r') as f:
             corpusTextArray.append(f.read().split())
 
     corpus = Corpus()
 
     corpus.fit(corpusTextArray, window=10)
 
-    glove = Glove(no_components=5, learning_rate=0.05)
-    glove.fit(corpus.matrix, epochs=30, no_threads=4, verbose=True)
-    glove.add_dictionary(corpus.dictionary)
-    glove.save('glove.txt')
+    model.fit(corpus.matrix, epochs=epochs, no_threads=4, verbose=False)
+    model.add_dictionary(corpus.dictionary)
+    return model
 
 
-# trainModel()
-corpusTextFileNameArray = os.listdir('corpora/methodCorpus/')
-corpusTextArray = []
-for corpusTextFileName in corpusTextFileNameArray:
-    with open('corpora/methodCorpus/' + corpusTextFileName, 'r') as f:
-        text = f.read()
-        corpusTextArray.append(text.split())
-        textTokens = word_tokenize(text)
-        print(textTokens)
-        textWithoutStopWords = [word for word in textTokens if not word in stopwords.words()]
-        print(textWithoutStopWords)
-        print("\n")
+def findClosestEmbeddings(wordEmbeddings, embedding):
+    return sorted(wordEmbeddings.keys(), key=lambda word: spatial.distance.euclidean(wordEmbeddings[word], embedding))
 
-model = Glove.load('glove.txt')
-print(model.word_vectors[model.dictionary['potato']])
 
+def removeStopWords():
+    corpusTextFileNameArray = os.listdir('corpora/methodCorpus/')
+    corpusTextArray = []
+    for corpusTextFileName in corpusTextFileNameArray:
+        with open('corpora/methodCorpus/' + corpusTextFileName, 'r') as f:
+            text = f.read()
+            corpusTextArray.append(text.split())
+            textTokens = word_tokenize(text)
+            textWithoutStopWords = [word for word in textTokens if not word in stopwords.words()]
+            separator = ' '
+            textWithoutStopWords = separator.join(textWithoutStopWords)
+            f.close()
+
+        with open('corpora/noStopwordsMethodCorpus/' + corpusTextFileName, 'w') as g:
+            g.write(textWithoutStopWords)
+
+
+def findNearestIngredients(wordEmbeddings, ingredient, maxIngredients=2):
+    closestEmbeddingsArray = findClosestEmbeddings(wordEmbeddings, wordEmbeddings[ingredient])[1:20]
+    closestIngredientsArray = []
+    for closestEmbeddings in closestEmbeddingsArray:
+        if closestEmbeddings in cleanIngredients:
+            closestIngredientsArray.append(closestEmbeddings)
+            # print(closestEmbeddings)
+
+        if len(closestIngredientsArray) >= maxIngredients:
+            return closestIngredientsArray
+
+    return closestIngredientsArray
+
+
+def formatWordEmbeddings(gloveModel):
+    wordEmbeddings = gloveModel.dictionary
+
+    for word in wordEmbeddings:
+        wordEmbeddings[word] = gloveModel.word_vectors[gloveModel.dictionary[word]]
+
+    return wordEmbeddings
+
+
+def gloveInit():
+
+    cleanIngredients = pd.read_csv('C:/Users/lipb1/Documents/Year 3 Bristol/MDM3/FOD/NLP/normalisedIngredients.csv')
+    cleanIngredients = np.array(cleanIngredients['Ingredients'])
+    gloveModel = Glove.load('models/glove 400.txt')
+    embeddings = formatWordEmbeddings(gloveModel)
+    return embeddings, cleanIngredients
+
+
+def queryGlove(embeddings, queryIngredient):
+    print('Glove output for \'' + queryIngredient + '\'')
+    print(findNearestIngredients(embeddings, queryIngredient, maxIngredients=3))
+
+
+# trainModel('corpora/noStopwordsMethodCorpus/', 3)
+'''
+nearestIngredientsArray = []
+
+modelTrained = False
+epoch = 20
+stepSize = 20
+gloveModel = Glove(no_components=30, learning_rate=0.05)
+# print('starting at epoch ' + str(epoch))
+# gloveModel = Glove.load('models/glove ' + str(epoch) + '.txt')
+while not modelTrained:
+    print('\ntraining up to ' + str(epoch) + ' epochs')
+    gloveModel = trainModel(gloveModel, 'corpora/noStopwordsMethodCorpus/', stepSize)
+    gloveModel.save('models/glove ' + str(epoch) + '.txt')
+    wordEmbeddings = gloveModel.dictionary
+
+    for word in wordEmbeddings:
+        wordEmbeddings[word] = gloveModel.word_vectors[gloveModel.dictionary[word]]
+
+    newNearestIngredientsArray = findNearestIngredients()
+    print(newNearestIngredientsArray)
+    if newNearestIngredientsArray == nearestIngredientsArray:
+        print('epoch: ')
+        print(epoch)
+        print(nearestIngredientsArray)
+        modelTrained = True
+
+    else:
+        nearestIngredientsArray = newNearestIngredientsArray
+        epoch += stepSize
+'''
